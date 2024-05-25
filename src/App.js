b32dfb66a2eaf97c66261877c954b9de1437a895
Janvii R V <114306374+JARVIS-28@users.noreply.github.com>
2024-05-25 13:23:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,283 @@
+// ParkingPage.js
+
+import React from 'react';
+import photo from './photo.jpeg'
+import photo1 from './photo1.jpeg'
+import video from './video.mp4'
+export default function  ParkingPage() {
+
+    return (
+      <html>
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="generator" content="Mobirise v5.9.13, a.mobirise.com" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+          <link rel="shortcut icon" href="assets/images/photo-1573710459621-bb101783ca0f.jpeg" type="image/x-icon" />
+          <meta name="description" content="Find nearby parking lots and get directions to park your vehicles hassle-free." />
+          <title>Find Parking Now</title>
+
+        
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/web/assets/mobirise-icons2/mobirise2.css?rnd=1713529916305" />
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/bootstrap/css/bootstrap.min.css?rnd=1713529916305" />
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/bootstrap/css/bootstrap-grid.min.css?rnd=1713529916305" />
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/bootstrap/css/bootstrap-reboot.min.css?rnd=1713529916305" />
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/dropdown/css/style.css?rnd=1713529916305" />
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/socicon/css/styles.css?rnd=1713529916305" />
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/theme/css/style.css?rnd=1713529916305" />
+          <link rel="preload" href="https://fonts.googleapis.com/css2?family=Onest:wght@400;700&display=swap&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+          <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Onest:wght@400;700&display=swap&display=swap" /></noscript>
+          <link rel="stylesheet" href="https://r.mobirisesite.com/397446/assets/css/mbr-additional.css?rnd=1713529916305" type="text/css" />
+        </head>
+
+        
+        
+        <div>
+        <section data-bs-version="5.1" className="menu menu2 cid-uaop2qgXBi" once="menu" id="menu-5-uaop2qgXBi">
+          <nav className="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+            <div className="container">
+              <div className="navbar-brand">
+                <span className="navbar-logo">
+                  <a>
+                    <img src={photo}  style={{height: '4.3rem'}} />
+                  </a>
+                </span>
+                <span className="navbar-caption-wrap"><a className="navbar-caption text-black display-4">FindMyLot</a></span>
+              </div>
+              
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true"><li className="nav-item">
+                    <a className="nav-link link text-black display-4" href="https://mobiri.se" aria-expanded="false">Directions</a>
+                  </li></ul>
+                <div className="navbar-buttons mbr-section-btn"><a className="btn btn-primary display-4" href="https://mobiri.se">Get Directions</a></div>
+              </div>
+            </div>
+          </nav>
+        </section>
+
+        <section data-bs-version="5.1" className="header18 cid-uaop2qhxV7 mbr-fullscreen" id="hero-15-uaop2qhxV7">
+          <video playsInline autoPlay muted loop id="bgvid" style={{position: 'absolute', width:'100%', left: '50%', top: '50%', height: '100%', objectFit: 'cover', transform: 'translate(-50%, -50%)', zIndex: '-1'}}>
+            <source src={video} type="video/mp4" />
+          </video>
+          <div className="mbr-overlay" style={{opacity: '0.5', backgroundColor: 'rgb(0, 0, 0)'}} />
+            <div className="container-fluid">
+              <div className="row">
+                <div className="content-wrap col-12 col-md-12">
+                  <h1 className="mbr-section-title mbr-fonts-style mbr-white mb-4 display-1">
+                    <strong>Park Easy</strong>
+                  </h1>
+                  <p className="mbr-fonts-style mbr-text mbr-white mb-4 display-7">FindMyLot - Your Ultimate Destination for Hassle-Free Parking Solutions. Say Goodbye to Parking Woes!</p>
+                  <div className="mbr-section-btn"><a className="btn btn-white-outline display-7" href="https://www.google.com">Get Directions</a></div>
+                  </div>
+              </div>
+            </div>
+        </section>
+
+        <section data-bs-version="5.1" className="article9 cid-uaop2qhqzn" id="about-us-9-uaop2qhqzn">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="card col-md-12 col-lg-10">
+                <div className="card-wrapper">
+                  <div className="card-content-text">
+                    <h3 className="card-title mbr-fonts-style mbr-white mt-3 mb-4 display-2">
+                      <strong><section id="about">About</section></strong>
+                    </h3>
+                    <div className="row card-box align-left">
+                      <div className="item features-without-image col-12">
+                        <div className="item-wrapper">
+                          <p className="mbr-text mbr-fonts-style display-7">Welcome to FindMyLot, your go-to solution for finding the perfect parking spot hassle-free!</p>
+                        </div>
+                      </div>
+                      <div className="item features-without-image col-12">
+                        <div className="item-wrapper">
+                          <p className="mbr-text mbr-fonts-style display-7">We specialize in making your parking experience a breeze, ensuring you never circle the block again.</p>
+                        </div>
+                      </div>
+                      <div className="item features-without-image col-12">
+                        <div className="item-wrapper">
+                          <p className="mbr-text mbr-fonts-style display-7">With us, parking stress is a thing of the past. Let's make parking easy and convenient for you!</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="image-wrapper d-flex justify-content-center mt-3">
+                    <img src={photo1} alt="Mobirise Website Builder" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section data-bs-version="5.1" className="article07 cid-uaop2qh3oN" id="generic-text-7-uaop2qh3oN">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="card col-md-12 col-lg-10">
+                <div className="card-wrapper">
+                  <h3 className="card-title mbr-fonts-style mbr-white mt-3 mb-4 display-2">
+                    <strong>FindMyLot - Your Parking Savior</strong>
+                  </h3>
+                  <div className="row card-box align-left">
+                    <div className="item features-without-image col-12">
+                      <div className="item-wrapper">
+                        <p className="mbr-text mbr-fonts-style display-7">Welcome to FindMyLot, your ultimate destination for hassle-free parking solutions! Say goodbye to circling the block endlessly in search of a spot.</p>
+                      </div>
+                    </div>
+                    <div className="item features-without-image col-12">
+                      <div className="item-wrapper">
+                        <p className="mbr-text mbr-fonts-style display-7">Our innovative platform is designed to swiftly locate the nearest parking lot for your precious vehicle. No more stress, just smooth parking experiences at your fingertips.</p>
+                      </div>
+                    </div>
+                    <div className="item features-without-image col-12">
+                      <div className="item-wrapper">
+                        <p className="mbr-text mbr-fonts-style display-7">Click the button below to embark on a journey towards stress-free parking adventures!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section data-bs-version="5.1" className="list1 cid-uaop2qipWx" id="faq-1-uaop2qipWx">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12 col-md-12 col-lg-10 m-auto">
+                <div className="content">
+                  <div className="row justify-content-center mb-5">
+                    <div className="col-12 content-head">
+                      <div className="mbr-section-head">
+                        <h4 className="mbr-section-title mbr-fonts-style align-center mb-0 display-2">
+                          <strong><section id="faq">Got Questions? We Have Answers!</section></strong>
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="bootstrap-accordion_7" className="panel-group accordionStyles accordion" role="tablist" aria-multiselectable="true">
+                    <div className="card">
+                      <div className="card-header" role="tab" id="headingOne">
+                        <a role="button" className="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core href="#collapse1_7" aria-expanded="false" aria-controls="collapse1">
+                          <h6 className="panel-title-edit mbr-semibold mbr-fonts-style mb-0 display-5">How does it work?</h6>
+                          <span className="sign mbr-iconfont mobi-mbri-arrow-down" />
+                        </a>
+                      </div>
+                      <div id="collapse1_7" className="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_7">
+                        <div className="panel-body">
+                          <p className="mbr-fonts-style panel-text display-7">FindMyLot uses cutting-edge technology to locate the nearest parking spots for your convenience. Just a few clicks and you're all set to park like a pro!</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card">
+                      <div className="card-header" role="tab" id="headingOne">
+                        <a role="button" className="panel-title collapsed" data-toggle="collapse" data-bs-toggle="collapse" data-core href="#collapse5_7" aria-expanded="false" aria-controls="collapse5">
+                          <h6 className="panel-title-edit mbr-semibold mbr-fonts-style mb-0 display-5">Need help finding us?</h6>
+                          <span className="sign mbr-iconfont mobi-mbri-arrow-down" />
+                        </a>
+                      </div>
+                      <div id="collapse5_7" className="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion" data-bs-parent="#bootstrap-accordion_7">
+                        <div className="panel-body">
+                          <p className="mbr-fonts-style panel-text display-7">Don't worry, we've got you covered. Our detailed directions will lead you straight to the parking paradise. Say goodbye to circling the block endlessly!</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section data-bs-version="5.1" className="social4 cid-uaop2qiH4O" id="follow-us-1-uaop2qiH4O">
+          <div className="container">
+            <div className="media-container-row">
+              <div className="col-12">
+                <h3 className="mbr-section-title align-center mb-5 mbr-fonts-style display-2">
+                  <strong>Stay Connected</strong>
+                </h3>
+                <div className="social-list align-center">
+                  <a className="iconfont-wrapper bg-facebook m-2 " target="_blank" href="https://mobiri.se">
+                    <span className="socicon-facebook socicon" />
+                  </a>
+                  <a className="iconfont-wrapper bg-twitter m-2" href="https://mobiri.se" target="_blank">
+                    <span className="socicon-twitter socicon" />
+                  </a>
+                  <a className="iconfont-wrapper bg-instagram m-2" href="https://mobiri.se" target="_blank">
+                    <span className="socicon-instagram socicon" />
+                  </a>
+                  <a className="iconfont-wrapper bg-tiktok m-2" href="https://mobiri.se" target="_blank">
+                    <span className="socicon-tiktok socicon" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section data-bs-version="5.1" className="contacts01 cid-uaop2qjvOU" id="contacts-1-uaop2qjvOU">
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-12 content-head">
+                <div className="mbr-section-head mb-5">
+                  <h3 className="mbr-section-title mbr-fonts-style align-center mb-0 display-2">
+                    <strong>Contact Us</strong>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="item features-without-image col-12 col-md-6 active">
+                <div className="item-wrapper">
+                  <div className="text-wrapper">
+                    <h6 className="card-title mbr-fonts-style mb-3 display-5">
+                      <strong>Phone</strong>
+                    </h6>
+                    <p className="mbr-text mbr-fonts-style display-7">
+                      <a href="tel:123-456-7890" className="text-black">123-456-7890</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="item features-without-image col-12 col-md-6">
+                <div className="item-wrapper">
+                  <div className="text-wrapper">
+                    <h6 className="card-title mbr-fonts-style mb-3 display-5">
+                      <strong>Email</strong>
+                    </h6>
+                    <p className="mbr-text mbr-fonts-style display-7">
+                      <a href="mailto:info@findmylot.com" className="text-black">info@findmylot.com</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section data-bs-version="5.1" className="footer3 cid-uaop2qjNV9" once="footers" id="footer-6-uaop2qjNV9">
+          <div className="container">
+            <div className="row">
+              <div className="row-links">
+                <ul className="header-menu">
+                  <li className="header-menu-item mbr-fonts-style display-5">
+                    <a href="#about" className="text-white">About</a>
+                  </li><li className="header-menu-item mbr-fonts-style display-5">
+                    <a href="#faq" className="text-white">FAQ</a>
+                  </li></ul>
+              </div>
+              <div className="col-12 mt-4">
+                <p className="mbr-fonts-style copyright display-7">© 2024 FindMyLot. All Rights Reserved.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      
+      </div>
+  
+  
+  </html>
+);
+
+
+  }
+
